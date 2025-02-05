9a6a0517a2415c68012e9fdf25bbab6ab4f9c14b
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-05 13:25:16 +0200

    створення моделі learning_logs

--- pcc_project/settings.py
+++ pcc_project/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Мої додатки
+    "learning_logs",
+    # Дефолтні джанго додатки
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
