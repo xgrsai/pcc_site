dea0b92e635b1ade68b027ab2d7cf814ae33e827
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-11 20:40:31 +0200

    додав адмінку
    додано адміністратора та адмінську сторінку

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from .models import Topic # Крапка перед models вказує Django шукати models.py в тому ж каталозі, що і admin.py
+
+admin.site.register(Topic)
 # Register your models here.
