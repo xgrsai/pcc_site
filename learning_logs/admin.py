26bbc04c5b9b48318a34f8b2b75ef823982e9df7
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-13 23:05:30 +0200

    class entry
    додано записи для тем

--- learning_logs/admin.py
+++ learning_logs/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Topic # Крапка перед models вказує Django шукати models.py в тому ж каталозі, що і admin.py
+from .models import Topic, Entry # Крапка перед models вказує Django шукати models.py в тому ж каталозі, що і admin.py
 
 admin.site.register(Topic)
-# Register your models here.
+admin.site.register(Entry)
+
