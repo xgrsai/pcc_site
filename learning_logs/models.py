16284e66aa7fd754468d0163a5b5eeb490316868
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-15 20:41:13 +0200

    зміна коментарів
    змінив коменти для пояснення моделей та class Meta

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models # модель - в БД це таблиця
 
 class Topic(models.Model):
     """A topic the user is learning about."""
@@ -15,7 +15,7 @@ class Entry(models.Model):
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
+    class Meta: # внутрішній клас у моделі, який використовується для налаштування її поведінки (як сортувати, як називати модель). З його допомогою можна змінювати порядок сортування записів, задавати назви таблиць у базі даних, визначати, як модель відображатиметься в адміністративній панелі тощо
         verbose_name_plural = 'entries'
         def __str__(self): # в іншому випадку ми виводитимемо не дані а предсталення об'єкта
             """Return a simple string representing the entry."""
