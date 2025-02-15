bbef48fc6fa82f78a385db26d35c17a1422b0a06
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-15 17:57:29 +0200

    сторінка з темами

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 app_name = 'learning_logs' # Змінна app_name допомагає Django відрізнити цей файл urls.py від файлів з такою ж назвою в інших додатках проекту
 urlpatterns = [
- # Home page
-path('', views.index, name='index'), # тут при пустому url основна сторінка це learning_logs | name='index': Дає маршруту унікальне ім’я. Це дозволяє звертатися до цього URL за іменем (наприклад, у шаблонах за допомогою тегу {% url 'learning_logs:index' %}) замість того, щоб жорстко прописувати URL-адресу.
+    # Home page
+    path('', views.index, name='index'), # тут при пустому url основна сторінка learning_logs в основному urls ||| name='index': Дає маршруту унікальне ім’я. Це дозволяє звертатися до цього URL за іменем (наприклад, у шаблонах за допомогою тегу {% url 'learning_logs:index' %}) замість того, щоб жорстко прописувати URL-адресу.
+    # Page that shows all topics.
+    path('topics/', views.topics, name='topics'), # можна без похилої риски головне щоб після нічого зайвого не було
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Topic
 
-def index(request):
- """The home page for Learning Log."""
- return render(request, 'learning_logs/index.html')
+def index(request): # request як http запит
+    """The home page for Learning Log."""
+    return render(request, 'learning_logs/index.html')
+
+def topics(request): 
+    """Show all topics."""
+    topics = Topic.objects.order_by('date_added') # запит до бази даних, запитуючи об'єкти Topic, відсортовані за атрибутом date_added
+    context = {'topics': topics} # django y render() просить словник
+    return render(request, 'learning_logs/topics.html', context) # context як словник тому django просить словник
