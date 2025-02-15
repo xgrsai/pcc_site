dccd1736ca14f8215bc67001e4349ef6b958b182
Yevhen Oleksyshyn <145606099+xgrsai@users.noreply.github.com>
2025-02-15 22:46:57 +0200

    додав форму для Topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     # Page that shows all topics.
     path('topics/', views.topics, name='topics'), # можна без похилої риски головне щоб після нічого зайвого не було
     # Detail page for a single topic.
-    path('topics/<int:topic_id>/', views.topic, name='topic'),
+    path('topics/<int:topic_id>/', views.topic, name='topic'), # другий аргумент звертається яку сторінку відображати
+    # Page for adding a new topic.
+    path('new_topic/', views.new_topic, name='new_topic'), # третій аргумент дозоляє зручноо посилатися на цей шлях в інших частинах проекту (зверхку написано -_-)
+
 
 ]
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
-def index(request): # request як http запит
+def index(request): # request як http запит (типу URL адреса в браузері)
     """The home page for Learning Log."""
     return render(request, 'learning_logs/index.html')
 
@@ -18,3 +19,19 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added') # мінус означає від найновіших до старіших
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TopicForm()
+    else:
+        # POST data submitted; process data.
+        form = TopicForm(data=request.POST) # аргумент передає значення полів форми
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
